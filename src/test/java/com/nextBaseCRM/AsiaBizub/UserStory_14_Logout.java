91b1c21ee324ecf6d1ee9564b41cc1440214ff3c
AsiaBizub <74011714+AsiaBizub@users.noreply.github.com>
2021-01-23 15:15:48 -0600

    6 test cases added

--- src/test/java/com/nextBaseCRM/AsiaBizub/UserStory_14_Logout.java
+++ src/test/java/com/nextBaseCRM/AsiaBizub/UserStory_14_Logout.java
@@ -2,12 +2,19 @@ package com.nextBaseCRM.AsiaBizub;
 
 public class UserStory_14_Logout {
 
+    //get url and verify title page
+    //login
+    //access profileAvatar
+    //logout
+    //verify title page
 
 
 
 }
 
 /*
+
+TC_G18-76
 User Story 14:
 As a user, I should be able to logout from the app
 
